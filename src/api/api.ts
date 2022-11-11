9c42b36460c9c1a18b3c0c3a56a221adb120cc49
laura-stack <81604596+laura-stack@users.noreply.github.com>
2022-11-11 13:08:52 -0600

    Dummy data is able to be pulled onto task list and is displayed for the user. Still needs to be formatted so that it is not displayed overtop of the header.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,43 +1,43 @@
-import * as mock from "./mocks";
-import {PetType, TaskType} from "./models";
+import * as mock from './mocks';
+import { PetType, TaskType } from './models';
 
 export default class Api {
-    private mocked = false;
+  private mocked = false;
 
-    constructor({ mocked } = { mocked: false }) {
-        this.mocked = mocked;
-    }
+  constructor({ mocked } = { mocked: false }) {
+    this.mocked = mocked;
+  }
 
-    /**
-     * Gets a pet from the database based on ID
-     * @param id
-     */
-    async getPet(id: number): Promise<PetType> {
-        return mock.getPet;
-    }
+  /**
+   * Gets a pet from the database based on ID
+   * @param id
+   */
+  async getPet(id: number): Promise<PetType> {
+    return mock.getPet;
+  }
 
-    /**
-     * Updates the pet in the database with the given payload
-     * @param id
-     * @param pet
-     */
-    async updatePet(id: string, pet: Partial<PetType>): Promise<PetType> {
-        return mock.updatePet;
-    }
+  /**
+   * Updates the pet in the database with the given payload
+   * @param id
+   * @param pet
+   */
+  async updatePet(id: string, pet: Partial<PetType>): Promise<PetType> {
+    return mock.updatePet;
+  }
 
-    /**
-     * Creates a new pet with the passed data and returns the newly created pet data
-     * @param pet
-     */
-    async createPet(pet: Omit<PetType, 'id'>): Promise<PetType> {
-        return mock.createPet;
-    }
+  /**
+   * Creates a new pet with the passed data and returns the newly created pet data
+   * @param pet
+   */
+  async createPet(pet: Omit<PetType, 'id'>): Promise<PetType> {
+    return mock.createPet;
+  }
 
-    /**
-     * Retrieve the task from the database
-     * @param id
-     */
-    async getTask(id: number): Promise<TaskType> {
-        return mock.getTask;
-    }
+  /**
+   * Retrieve the task from the database
+   * @param id
+   */
+  async getTask(id: number): Promise<TaskType> {
+    return mock.getTask;
+  }
 }
--- src/api/mocks.ts
+++ src/api/mocks.ts
@@ -2,18 +2,18 @@
 Placeholder data for API functions to grab.
  */
 
-import {PetType, TaskType} from "./models";
+import { PetType, TaskType } from './models';
 
 export const getPet: PetType = {
-    id: 1,
-    userid: 1,
-    name: 'Billy',
-    health: 100,
-    species: 'cat',
-    color: 'red',
-    status: 'happy',
-    age: 'adult',
-    accessories: ['tophat', 'bowtie'],
+  id: 1,
+  userid: 1,
+  name: 'Billy',
+  health: 100,
+  species: 'cat',
+  color: 'red',
+  status: 'happy',
+  age: 'adult',
+  accessories: ['tophat', 'bowtie'],
 };
 
 export const updatePet: PetType = getPet;
@@ -21,19 +21,19 @@ export const updatePet: PetType = getPet;
 export const createPet: PetType = getPet;
 
 export const getTask: TaskType = {
-    id: 1,
-    userid: 1,
-    name: 'Wash dishes',
-    description: 'Put dirty dishes in dishwasher, then run it.',
-    repeat: false,
-    done: false,
-    date: '2022-31-10',
+  id: 1,
+  userid: 1,
+  name: 'Wash dishes',
+  description: 'Put dirty dishes in dishwasher, then run it.',
+  repeat: false,
+  done: false,
+  date: '2022-31-10',
 };
 
 export const createTask: TaskType = getTask;
 
 export const getUser: { password: string; name: string; id: number } = {
-    id: 1,
-    name: 'usermcuserface',
-    password: 'superstrongpw69',
+  id: 1,
+  name: 'usermcuserface',
+  password: 'superstrongpw69',
 };
