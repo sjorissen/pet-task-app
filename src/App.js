fd926aaf62013a8e052977312b5eed6aaf874e84
Jhoffman7 <jhoffman7@my.apsu.edu>
2022-11-16 12:23:37 -0600

    11/16/2022 This iteration is where I began development of the main webpage and its css, so far we have the navbar implemented and I have set up a two column page setup.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 // import logo from './logo.svg';
-import NewTask from './tasks/taskList';
-import { TaskToScreen } from './tasks/taskList';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import EventCalendar from './calendar/EventCalendar';
+// import { NewTask } from './tasks/taskList';
 import './App.css';
+import './styles/mainPage.css';
 
 function App() {
+  {
+    return (
+      //==================== This is for the top navbar ===================
+      // eslint-disable-next-line react/jsx-filename-extension
+      <>
+        {/*//==================== This is for the top navbar =================== //*/}
+        {/*eslint-disable-next-line react/jsx-filename-extension*/}
+        <Box id="navbar">
+          <AppBar id="appBar">
+            <Toolbar>
+              <Typography id="furryMinder" variant="h6" component="div">
+                Furry-Minder
+              </Typography>
+              <Button id="login">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        {/*//This will be for the layout of the main webpage, should be a responsive, 2 column format*/}
+        <div class="row">
+          <div class="column left">
+            <h2>Column 1</h2>
+            <p>Some text..</p>
+          </div>
+          <div class="column right">
+            <h2>Column 2</h2>
+            <p>Some text..</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   // eslint-disable-next-line react/jsx-filename-extension
-
-  return (
-    // eslint-disable-next-line react/jsx-filename-extension
-
-    // eslint-disable-next-line react/jsx-filename-extension
-    <>
-      <NewTask />
-      <TaskToScreen />
-    </>
-
-    /*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>*/
-  );
+  //return <EventCalendar />;
 }
 
 export default App;
